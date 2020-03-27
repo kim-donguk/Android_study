fa23792e33c4daffe2948d396e28e51d0c686508
Myunghan Jeong <myunghanjeong@Myunghanui-iMac.local>
2020-03-27 16:06:51 +0900

    3day study

---
+++ MylayoutInflater/app/src/main/java/org/techtown/mylayoutinflater/MainActivity.java
@@ -0,0 +1,32 @@
+package org.techtown.mylayoutinflater;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    FrameLayout container;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        container = (FrameLayout) findViewById(R.id.container);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+                //레이아웃 인플레이션을 하면 view를 객체화하여 사용할 수 있다.
+
+            }
+        });
+    }
+}
