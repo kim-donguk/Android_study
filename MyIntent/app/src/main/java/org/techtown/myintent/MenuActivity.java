c5464d029531eb729ed2386971cdf21cf4e947e0
Myunghan Jeong <myunghanjeong@Myunghanui-iMac.local>
2020-03-28 17:24:49 +0900

    4day study

---
+++ MyIntent/app/src/main/java/org/techtown/myintent/MenuActivity.java
@@ -0,0 +1,31 @@
+package org.techtown.myintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+
+                setResult(Activity.RESULT_OK, intent);
+
+                finish();
+            }
+        });
+    }
+}
---
+++ MyParcelable/app/src/main/java/org/techtown/myparcelable/SimpleData.java
@@ -0,0 +1,38 @@
+package org.techtown.myparcelable;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class SimpleData implements Parcelable {
+
+    int number;
+    String message;
+
+    public SimpleData(int number, String message) {
+        this.number = number;
+        this.message = message;
+    }
+    public SimpleData(Parcel src){
+        number = src.readInt();
+        message = src.readString();
+    }
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator(){
+        public SimpleData createFromParcel(Parcel in){
+            return new SimpleData(in);
+        }
+        public SimpleData[] newArray(int size){
+            return new SimpleData[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(number);
+        parcel.writeString(message);
+    }
+}
